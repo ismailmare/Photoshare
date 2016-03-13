c7ae00b23181a1667433cca021a1cecff3f8564f
hughboi <hdc@ualberta.ca>
2016-03-13 13:53:48 -0600

    changed the database_info.txt to a file called config.php that contains an array of config settings including the database info. If we want we can just add the username and pasword of oracle account we are using.

---
+++ include/config.php
@@ -0,0 +1,23 @@
+<?php
+/*
+  host = gwynne.cs.ualberta.ca
+  port = 1521
+  SID = CRS
+
+  This config file should be included on every page of the project,
+  or atleast any page you want access to these settings. Feel free
+  to add anything to the config array if you need to have global settings
+  or things like paths to commonly used resources for the project (e.g.,
+  log files, upload directories, resources).
+
+*/
+ 
+$config = array(
+	"db" => array(
+		"host" => "gwynne.cs.ualberta.ca",
+		"port" => 1521,
+		"SID" => "CRS",
+	)
+);
+
+?>
