95bb875e3e3eb7318b7ef9e9d262a9112cd51ff0
Ismail Mare <imare@cs.ualberta.ca>
2016-03-18 20:05:42 -0600

    Signin and login Html/JS code

--- usermanagement/login.php
+++ usermanagement/login.php
@@ -1,47 +1,58 @@
-<?php
-/**
-    * This page is meant to allow clients to login as a registered user
-    * and perform various tasks.
-    
-    * @date March 18, 2016
-*/
-?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-        
-	    <!-- Latest compiled and minified CSS -->
-	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" 
-            integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
 
-	    <!-- Optional theme -->
-	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" 
-            integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" 	crossorigin="anonymous">
+    <title>Signin Template for Bootstrap</title>
 
-	    <!-- Latest compiled and minified JavaScript -->
-	    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" 
-            integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    </head>
-    
-    <body>
-        <div class="container">
-            <form class="form-signin">
-            <h2 class="form-signin-heading">Please sign in</h2>
-            <label for="inputEmail" class="sr-only">Email address</label>
-            <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-            <label for="inputPassword" class="sr-only">Password</label>
-            <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-            <div class="checkbox">
-                <label>
-                <input type="checkbox" value="remember-me"> Remember me
-            </label>
-            </div>
-            <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-            </form>
+    <!-- Bootstrap core CSS -->
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="signin.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body background="/~imare/include/images/bgimage.jpg" >
+
+    <div class="container">
+
+      <form class="form-signin">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me"> Remember me
+          </label>
         </div>
-    </body>
- </html>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      </form>
+
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
+
--- usermanagement/signup.php
+++ usermanagement/signup.php
@@ -1,55 +1,98 @@
 <?php
-require_once "include/db.php";
+/**
+    * This page is meant to allow clients to login as a registered user
+    * and perform various tasks.
+    
+    * @date March 18, 2016
+*/
 ?>
-
+<!DOCTYPE html>
 <html>
     <head>
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        
+	    <!-- Latest compiled and minified CSS -->
+	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" 
+            integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
-	<!-- Optional theme -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" 	crossorigin="anonymous">
+	    <!-- Optional theme -->
+	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" 
+            integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" 	crossorigin="anonymous">
 
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	    <!-- Latest compiled and minified JavaScript -->
+	    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" 
+            integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     </head>
-
-    <body>
-      <div id="login-overlay" class="modal-dialog">
-      <div class="modal-content">
-          <div class="modal-body">
-              <div class="row">
-                  <div class="col-xs-6">
-                      <div class="well">
-                          <form id="loginForm" method="POST">
-                              <div class="form-group">
-                                  <label for="username" class="control-label">Username</label>
-                                  <input type="text" class="form-control" name="username" value="" required="" title="Please enter your username" placeholder="username">
-                                  <span class="help-block"></span>
-                              </div>
-                              <div class="form-group">
-                                  <label for="password" class="control-label">Password</label>
-                                  <input type="password" class="form-control" name="password" placeholder="password" value="" required="" title="Please enter your password">
-                                  <span class="help-block"></span>
-                              </div>
-                              <div id="loginErrorMsg" class="alert alert-error hide">Wrong username or password</div>
-                              <div class="checkbox">
-                                  <label>
-                                      <input type="checkbox" name="remember" id="remember"> Remember login
-                                  </label>
-                                  <p class="help-block">(if this is a private computer)</p>
-                              </div>
-                              <button type="submit" value="login" name="submit" class="btn btn-success btn-block">Login</button>
-                          </form>
-                      </div>
-                  </div>
-                  <div class="col-xs-6">
-                      <p class="lead">Register now.</p>
-                      <p><a href="signup.php" class="btn btn-info btn-block">Yes please, register now!</a></p>
-                  </div>
-              </div>
-          </div>
-      </div>
-      </div>
+    
+    <body background="/~imare/include/images/bgimage.jpg" >
+        <div class="container">
+            
+            <form class="form-signin">
+            <h2 class="form-signin-heading">Please Register</h2>
+                
+                <div style="float:left;margin-right:20px;">
+                    <label for="inputUsername" class="label_defult"> </label> 
+                    <input type="username" id="inputUser"name class="form-control" placeholder="Username" required autofocus>
+                </div>
+                <div style="float:left;">
+                    <label for="inputFirstname" class="label_defult"> </label>
+                    <input type="Firstname" id="inputFirstname" class="form-control" placeholder="First Name" required autofocus>
+                </div>
+                
+                
+                <br></br>
+                <br style="clear:both;" />
+                <div style="float:left;margin-right:20px;">
+                    <label for="inputLastname" class="label_defult"> </label>
+                    <input type="Lastname" id="inputLastname" class="form-control" placeholder="Last Name" required autofocus>
+                </div>
+                <div style="float:left;">
+                    <label for="inputEmail" class="label_defult"> </label>
+                    <input type="Email" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
+                </div>
+            
+            
+                <br></br>
+                <br style="clear:both;" />
+                <div style="float:left;margin-right:20px;">
+                    <label for="inputLastname" class="label_defult"> </label>
+                    <input type="Lastname" id="inputLastname" class="form-control" placeholder="Last Name" required autofocus>
+                </div>
+            
+                <div style="float:left;">
+                    <label for="inputAddress" class="label_defult"> </label>
+                    <input type="Address" id="inputAddress" class="form-control" placeholder="Address" required>
+                </div>
+                <br></br>
+            
+                <br style="clear:both;" />
+                <div style="float:left;margin-right:20px;">
+                    <label for="inputPhone" class="label_defult"></label>
+                    <input type="number_format" id="inputPhone" class="form-control" placeholder="Phone" required autofocus>
+                </div>
+                <br></br>
+            
+                <br style="clear:both;" />
+                <div style="float:left;margin-right:20px;">
+                    <label for="inputPassword1" class="label_defult"></label>
+                    <input type="password" id="inputPassword1" class="form-control" placeholder="Password" data-minlength="6" required autofocus>
+                    <div class="help-block">Minimum of 6 characters</div>
+                </div>
+            
+                <div style="float:left;">
+                    <label for="inputPassword2" class="label_defult"></label>
+                    <input type="password" id="inputPassword2" class="form-control" placeholder="Confirm" data-minlength="6" required autofocus>
+                    <div class="help-block">Minimum of 6 characters</div>
+                </div>
+            
+            
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
+            </form>
+            </center>
+        </div>
+        </div>
     </body>
-</html>
+ </html>
