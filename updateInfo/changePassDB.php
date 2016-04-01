06bcd25dd01357c0a03f322c0cbe56e4174db3c1
ismailmare <ismailmare@gmail.com>
2016-04-01 17:19:11 -0600

    just got rid of garbage

--- updateInfo/changePassDB.php
+++ updateInfo/changePassDB.php
@@ -16,19 +16,8 @@
 	$sql2 = 'UPDATE admin SET PASSWORD = \''.$pass2.'\' WHERE USER_NAME=\''.$username.'\'';
 	$newDB->executeStatement($sql2);
     } 
-    //oci_bind_by_name($stid, ':firstname', $firstname);
-    //oci_bind_by_name($stid, ':lastname', $lastname);
-    //oci_bind_by_name($stid, ':address', $address);
-    //oci_bind_by_name($stid, ':phone', $phone);
-    //oci_bind_by_name($stid, ':email', $email);
-    //oci_bind_by_name($stid, ':Username', $username);
-
-//    $sql = oci_parse($newDB->getConnection(), $sql1);
-//    oci_execute($sql,OCI_COMMIT_ON_SUCCESS);
-//    oci_free_statement($sql); 
 
     $newDB->executeStatement($sql1);    
-    //oci_free_statement($stid);
     $_SESSION['success'] = 'passchanged';	
     header("Location: ../profile.php");
     }
--- updateInfo/updateinfoDB.php
+++ updateInfo/updateinfoDB.php
@@ -11,19 +11,7 @@ $email = ($_POST["inputEmail"]);
     
 	$sql1 = 'UPDATE persons SET first_name = \''.$firstname.'\', last_name = \''.$lastname.'\', address = \''.$address.'\', phone = \''.$phone.'\', email= \''.$email.'\' WHERE user_name = \''.$username.'\'';
    
-    //$stid = oci_parse($newDB->getConnection(), $sql1);
- 
-    //oci_bind_by_name($stid, ':firstname', $firstname);
-    //oci_bind_by_name($stid, ':lastname', $lastname);
-    //oci_bind_by_name($stid, ':address', $address);
-    //oci_bind_by_name($stid, ':phone', $phone);
-    //oci_bind_by_name($stid, ':email', $email);
-    //oci_bind_by_name($stid, ':Username', $username);
-
-    $newDB->executeStatement($sql1);
-    //$res = oci_execute($stid, OCI_COMMIT_ON_SUCCESS); 
-    
-    //oci_free_statement($stid);	
+    $newDB->executeStatement($sql1);	
     header("Location: ../profile.php");
     $_SESSION['success']='updated';
     exit();
